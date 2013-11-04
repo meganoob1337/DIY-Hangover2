ba8afd4eb8ebbd8169bebeb0418287053a2cc3b9
Arne Feil <git@arnefeil.de>
2013-11-05 00:45:04 +0100

    ups

--- DIY-Hangover/build/source/buildConfig/debug/de/arnefeil/diyhangover/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package de.arnefeil.diyhangover;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
